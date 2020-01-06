c244a07047049b6724ec55069a646a72c2f11560
chenguangdai <40900789+chenguangdai@users.noreply.github.com>
2020-01-05 22:44:41 -0500

    Update linear_model.R

--- Linear model/linear_model.R
+++ Linear model/linear_model.R
@@ -48,69 +48,111 @@ analys <- function(mm, ww, q){
   return(selected_feature_index)
 }
 
-
+### data-splitting methods (DS and MDS)
 DS <- function(X, y, num_split, q){
-  n = dim(X)[1]; p = dim(X)[2]
-  inclusion_rate_multiple <- matrix(0, nrow = num_split, ncol = p)
-  fdr_multiple <- rep(0, num_split)
-  power_multiple <- rep(0, num_split)
+  n <- dim(X)[1]; p <- dim(X)[2]
+  inclusion_rate <- matrix(0, nrow = num_split, ncol = p)
+  fdp <- rep(0, num_split)
+  power <- rep(0, num_split)
   num_select <- rep(0, num_split)
   for(iter in 1:num_split){
-    ### randomly split the data and estimate coefficient
+    ### randomly split the data
     sample_index1 <- sample(x = c(1:n), size = 0.5 * n, replace = F)
     sample_index2 <- setdiff(c(1:n), sample_index1)
-    ### get penalty lambda
+    ### get the penalty lambda for Lasso
     cvfit <- cv.glmnet(X[sample_index1, ], y[sample_index1], type.measure = "mse", nfolds = 10)
     lambda <- cvfit$lambda.1se
+    ### run Lasso on the first half of the data
     beta1 <- as.vector(glmnet(X[sample_index1, ], y[sample_index1], family = "gaussian", alpha = 1, lambda = lambda)$beta)
     nonzero_index <- which(beta1 != 0)
+    ### run OLS on the second half of the data, restricted on the selected features
     beta2 <- rep(0, p)
     beta2[nonzero_index] <- as.vector(lm(y[sample_index2] ~ X[sample_index2, nonzero_index] - 1)$coeff)
     
-    ### calculate the test statistics
+    ### calculate the mirror statistics
     M <- abs(beta1 + beta2) - abs(beta1 - beta2)
-    current_selected_index <- analys(M, abs(M), q)
+    selected_index <- analys(M, abs(M), q)
     ### number of selected variables
-    num_select[iter] <- length(current_selected_index)
-    inclusion_rate_multiple[iter, current_selected_index] <- 1/num_select[iter]
-    ### false discovery rate
-    fdr_multiple[iter] <- (length(current_selected_index) - length(intersect(current_selected_index, signal_index)))/num_select[iter]
+    num_select[iter] <- length(selected_index)
+    inclusion_rate[iter, selected_index] <- 1/num_select[iter]
+    ### false discovery proportion
+    fdp[iter] <- (length(selected_index) - length(intersect(selected_index, signal_index)))/num_select[iter]
     ### power
-    power_multiple[iter] <- length(intersect(current_selected_index, signal_index))/p0
-    
+    power[iter] <- length(intersect(selected_index, signal_index))/p0
   }
-  ### single splitting result
-  single_split_fdr <- fdr_multiple[1]
-  single_split_power <- power_multiple[1]
-  ### multiple splitting result
-  inclusion_rate <- apply(inclusion_rate_multiple, 2, mean)
+  
+  ### single data-splitting (DS) result
+  single_split_fdp <- fdp[1]
+  single_split_power <- power[1]
+  
+  ### multiple data-splitting (MDS) result
+  inclusion_rate <- apply(inclusion_rate, 2, mean)
+  ### rank the features by the empirical inclusion rate
   feature_rank <- order(inclusion_rate)
   feature_rank <- setdiff(feature_rank, which(inclusion_rate == 0))
-  null_variable <- numeric()
+  null_feature <- numeric()
+  ### backtracking 
   for(feature_index in 1:length(feature_rank)){
     if(sum(inclusion_rate[feature_rank[1:feature_index]]) > q){
       break
     }else{
-      null_variable <- c(null_variable, feature_rank[feature_index])
+      null_feature <- c(null_feature, feature_rank[feature_index])
     }
   }
-  ### conservative one
-  selected_index <- setdiff(feature_rank, null_variable)
-  multiple_splitting_fdr <- (length(selected_index) - length(intersect(selected_index, signal_index)))/length(selected_index)
-  multiple_splitting_power <- length(intersect(selected_index, signal_index))/p0
+  selected_index <- setdiff(feature_rank, null_feature)
+  multiple_split_fdp <- (length(selected_index) - length(intersect(selected_index, signal_index)))/length(selected_index)
+  multiple_split_power <- length(intersect(selected_index, signal_index))/p0
   
-  list(SDS_fdr = single_split_fdr, SDS_power = single_split_power, MDS_fdr = multiple_splitting_fdr, MDS_power = multiple_splitting_power)
+  list(DS_fdp = single_split_fdp, DS_power = single_split_power, MDS_fdp = multiple_split_fdp, MDS_power = multiple_split_power)
 }
 
-## Knockoff Result
-KN = function(X, y, q){
+### model-X knockoff filter (Candes et al. 2018)
+M_knockoff <- function(X, y, q){
   knockoff_result <- knockoff.filter(X, y, fdr = q, offset = 0)
   selected_index <- knockoff_result$selected
+  ### false discovery rate
+  knockoff_fdp <- (length(selected_index) - length(intersect(selected_index, signal_index)))/length(selected_index)
   ### power
-  knockoff_fdr <- (length(selected_index) - length(intersect(selected_index, signal_index)))/length(selected_index)
   knockoff_power <- length(intersect(selected_index, signal_index))/p0
-  list(fdr = knockoff_fdr, power = knockoff_power)
+  list(fdp = knockoff_fdp, power = knockoff_power)
 }
+
+### fixed-design knockoff filter based on data splitting and data recycling (Barbers and Candes 2019)
+F_knockoff <- function(X, y, q){
+  ### randomly split the data
+  sample_index1 <- sample(x = c(1:n), size = 0.5 * n, replace = F)
+  sample_index2 <- setdiff(c(1:n), sample_index1)
+  ### feature screening via Lasso
+  fit <- glmnet(X[sample_index1, ], y[sample_index1], family = "gaussian", alpha = 1)
+  beta1_mat <- fit$beta
+  lambda <- fit$lambda
+  final_lambda <- numeric(p)
+  for(i in 1:p){
+    final_lambda[i] <- lambda[max(which(beta1_mat[i,] == 0))]
+  }
+  sorted_lambda <- sort(final_lambda, decreasing = T, index.return = T)
+  nonzero_index <- sorted_lambda$ix[1:trunc(n/4)]
+  ### create knockoff features
+  X2_knockoff <- create.fixed(X[sample_index2, nonzero_index])$X
+  X1_knockoff <- X[sample_index1, nonzero_index]
+  X_knockoff <- rbind(X1_knockoff, X2_knockoff)
+  X1 <- X[sample_index1, nonzero_index]
+  X2 <- X[sample_index2, nonzero_index]
+  X <- rbind(X1, X2)
+  y1 <- y[sample_index1]
+  y2 <- y[sample_index2]
+  y <- c(y1, y2)
+  
+  M <- stat.glmnet_coefdiff(X, X_tilde, y)
+  cutoff <- knockoff.threshold(M, fdr = q, offset = 0)
+  selected_index <- nonzero_index[sort(which(M >= cutoff))]
+  inc <- intersect(signal_index, selected_index)
+  td <- length(inc)
+  fdp <- (length(nz_est) - td)/max(length(nz_est), 1)
+  power <- td/length(signal_index)
+  list(fdp = fdp, power = power)
+}
+
 
 BH_and_BY_single = function(X, y, q){
   #### get penalty lambda
@@ -172,38 +214,6 @@ BH_and_BY_multiple = function(X, y, q, num_split){
   return(list(BH_fdr = BH_fdr, BH_power = BH_power, BY_fdr = BY_fdr, BY_power = BY_power))
 }
 
-KN_hdi = function(X, y, q){
-  sample_index1 <- sample(x = c(1:n), size = 0.5 * n, replace = F)
-  sample_index2 <- setdiff(c(1:n), sample_index1)
-  fit = glmnet(X[sample_index1, ], y[sample_index1], family = "gaussian", alpha = 1)
-  beta1_mat = fit$beta
-  lambda = fit$lambda
-  final_lambda = numeric(p)
-  for(i in 1:p){
-    final_lambda[i] = lambda[max(which(beta1_mat[i,]==0))]
-  }
-  sorted_lambda = sort(final_lambda, decreasing = T, index.return = T)
-  nonzero_index1 = sorted_lambda$ix[1:trunc(n/4)]
-  X2_tilde = create.fixed(X[sample_index2, nonzero_index1])$X
-  X1_tilde = X[sample_index1,nonzero_index1]
-  X_tilde = rbind(X1_tilde, X2_tilde)
-  X1 = X[sample_index1, nonzero_index1]
-  X2 = X[sample_index2, nonzero_index1]
-  X = rbind(X1, X2)
-  y1 = y[sample_index1]
-  y2 = y[sample_index2]
-  y = c(y1,y2)
-  
-  M <- stat.glmnet_coefdiff(X, X_tilde, y)
-  t = knockoff.threshold(M, fdr = q, offset = 0)
-  selected = sort(which(M >= t))
-  nz_est = nonzero_index1[selected]
-  inc = intersect(signal_index, nz_est)
-  td = length(inc)
-  fdp = (length(nz_est)-td)/max(length(nz_est),1)
-  power = td/length(signal_index)
-  list(fdp = fdp, power = power)
-}
 
 
 ###
@@ -212,18 +222,4 @@ knockoff_result <- KN(X, y, q)
 BH_result <- BH_and_BY_single(X, y, q)
 BH_multiple_result <- BH_and_BY_multiple(X, y, q, num_split)
 knockoff_split_result <- KN_hdi(X, y, q)
-
-
-### save data
-data_save <- list(single_split_fdr = DS_result$SDS_fdr, single_split_power = DS_result$SDS_power,
-                  multiple_splitting_fdr = DS_result$MDS_fdr,
-                  multiple_splitting_power = DS_result$MDS_power,
-                  knockoff_fdr = knockoff_result$fdr, knockoff_power = knockoff_result$power, 
-                  BH_fdr = BH_result$BH_fdr, BH_power = BH_result$BH_power, 
-                  BY_fdr = BH_result$BY_fdr, BY_power = BH_result$BY_power,
-                  BH_multiple_fdr = BH_multiple_result$BH_fdr, BH_multiple_power = BH_multiple_result$BH_power,
-                  BY_multiple_fdr = BH_multiple_result$BY_fdr, BY_multiple_power = BH_multiple_result$BY_power,
-                  knockoff_split_fdr = knockoff_split_result$fdp,
-                  knockoff_split_power = knockoff_split_result$power)
-save(data_save, file = paste("/n/home09/cdai/FDR/result/LinearHighConstantCorrelation/delta_", delta, "_replicate_", replicate, ".RData", sep = ""))
 
